b419637ec0c64691d23df430c6d084bd8e65af67
Christianib003 <christianib003@gmail.com>
2024-06-03 16:56:35 +0200

    add post method for creating a new store

--- app.py
+++ app.py
@@ -1,4 +1,31 @@
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
+
+stores = [
+    {
+        "name": "My Wonderful Store",
+        "items": [
+            {
+                "name": "My Item",
+                "price": 15.99
+            },
+            {
+                "name": "My Item 2",
+                "price": 10.99
+            }
+        ]
+    }
+]
+
+@app.route('/stores', methods=['GET'])
+def get_stores():
+    return {"stores": stores}
+
+@app.route('/stores', methods=['POST'])
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data['name'], "items": []}
+    stores.append(new_store)
+    return new_store, 201
