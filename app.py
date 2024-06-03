6f405eb7fe91896baad0454cee65b8c0fd3f872c
Christianib003 <christianib003@gmail.com>
2024-06-03 17:13:08 +0200

    add method to add new item to a given store

--- app.py
+++ app.py
@@ -29,3 +29,16 @@ def create_store():
     new_store = {"name": request_data['name'], "items": []}
     stores.append(new_store)
     return new_store, 201
+
+@app.route('/stores/<string:name>/item', methods=['POST'])
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {
+                "name": request_data["name"],
+                "price": request_data["price"]
+                }
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404
