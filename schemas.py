37b07c7a72e09fb66f97f654f995e649a105eee4
Christianib003 <christianib003@gmail.com>
2024-06-04 20:05:15 +0200

    add basic schemas

--- schemas.py
+++ schemas.py
@@ -1,3 +1,13 @@
 from marshmallow import Schema, fields
 
 
+class RequestSchema(Schema):
+    request_id = fields.Str(dump_only=True)
+    amount = fields.Str(required=True)
+    status = fields.Str(required=True)
+    household_id = fields.Str(required=True)
+
+class HouseholdSchema(Schema):
+    household_id = fields.Str(dump_only=True)
+    area = fields.Str(required=True)
+    address = fields.Str(required=True)
