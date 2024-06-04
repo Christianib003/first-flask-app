0d41da09ad80947e9674b57291e8c9d99cde1f47
Christianib003 <christianib003@gmail.com>
2024-06-04 17:43:25 +0200

    add method to create new household

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
@@ -21,8 +21,25 @@ households = [
 def get_households():
     """
     Retrieves the list of households.
-    
+
     Returns:
         dict: A dictionary containing the list of households.
     """
     return {"households": households}
+
+@app.post("/households")
+def create_household():
+    """
+    Creates a new household.
+
+    Returns:
+        dict: A dictionary containing the newly created household.
+    """
+    request_data = request.get_json()
+    new_household = {
+        "house_number": request_data["house_number"],
+        "area": request_data["area"],
+        "requests": []
+    }
+    households.append(new_household)
+    return new_household
