e0328e3fd4262906504fb69cf9cdb56365063ccd
Christianib003 <christianib003@gmail.com>
2024-06-04 17:35:09 +0200

    create households and their request

--- app.py
+++ app.py
@@ -2,3 +2,17 @@ from flask import Flask
 
 
 app = Flask(__name__)
+
+
+households = [
+    {
+        "house_number": "KK 123",
+        "area": "Kicukiro",
+        "requests": [
+            {
+                "amount": 5,
+                "is_completed": False
+            }
+        ]
+    }
+]
